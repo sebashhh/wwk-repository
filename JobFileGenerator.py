3902ced8441f7d27306037ac82259dacb9ba6d22
Sebastian Caparas <sjcapa19@gmail.com>
2021-06-14 13:08:50 -0400

    copy/paste should make the code run 3 roots?

--- JobFileGenerator.py
+++ JobFileGenerator.py
@@ -27,10 +27,13 @@ functional = ""
 basisSet = "6-31+G*"
 title1 = "Funcs"
 title2 = "Funcs"
+title3 = "Funcs"
 fileData1 = ""
 fileData2 = ""
+fileData3 = ""
 shData1 = ""
 shData2 = ""
+shData3 = ""
 romanNumeral = ""
 
 #loads the GJF template from the local directory
@@ -38,12 +41,16 @@ with open("!GaussianInputJFG.gjf", 'r') as file :
     fileData1 = file.read()
 with open("!GaussianInputJFG.gjf", 'r') as file :
     fileData2 = file.read()
+with open("!GaussianInputJFG.gjf", 'r') as file :
+    fileData3 = file.read()
 #loads the gaussian.sh job submission template from the local directory
 with open("!gaussianJob.sh", 'r') as file2 :
     shData1 = file2.read()
 with open("!gaussianJob.sh", 'r') as file2 :
     shData2 = file2.read()
-    
+with open("!gaussianJob.sh", 'r') as file2 :
+    shData3 = file2.read()
+
 isConfiguring = True
 while isConfiguring:
     print(tint("Default Functional: ", bcolors.CYAN) + functional)
@@ -69,29 +76,45 @@ while isConfiguring:
     if(theInput.lower() == "ok"):
         title1 += functional + romanNumeral + "1st"
         title2 += functional + romanNumeral + "2nd"
+        title3 += functional + romanNumeral + "3rd"
     
         fileData1 = fileData1.replace('TITLE', title1)
         fileData2 = fileData2.replace('TITLE', title2)
+        fileData3 = fileData3.replace('TITLE', title3)
+        
         fileData1 = fileData1.replace('ROOT', 'root=1')
         fileData2 = fileData2.replace('ROOT', 'root=2')
+        fileData3 = fileData3.replace('ROOT', 'root=3')
+        
         fileData1 = fileData1.replace('FUNCTIONAL', functional)
         fileData2 = fileData2.replace('FUNCTIONAL', functional)
+        fileData3 = fileData3.replace('FUNCTIONAL', functional)
+        
         fileData1 = fileData1.replace('BASIS_SET', basisSet)
         fileData2 = fileData2.replace('BASIS_SET', basisSet)
+        fileData3 = fileData3.replace('BASIS_SET', basisSet)
+        
         shData1 = shData1.replace('TITLE', title1)
         shData2 = shData2.replace('TITLE', title2)
+        shData3 = shData3.replace('TITLE', title3)
         #Creates root1.gjf
         with open(title1 + ".gjf", 'w') as file:
             file.write(fileData1)
         #Creates root2.gjf    
         with open(title2 + ".gjf", 'w') as file:
             file.write(fileData2)
+        #Creates root3.gjf
+        with open(title3 + ".gjf", 'w') as file:
+            file.write(fileData3)
         #Creates root1.sh
         with open(title1 + ".sh", 'w') as file2:
             file2.write(shData1)
         #Creates root2.sh    
         with open(title2 + ".sh", 'w') as file2:
             file2.write(shData2)
+        #Creates root3.sh  
+        with open(title3 + ".sh", 'w') as file2:
+            file2.write(shData3)
         isConfiguring = False
     else:
         print("So...you're not ready.")
