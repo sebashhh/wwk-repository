eca9fefa214c6b54faf497f16b56439891b27979
sebashhh <sjcapa19@gmail.com>
2021-06-22 13:44:04 -0400

    qchem script changes

--- QChemJobFileGenerator.py
+++ QChemJobFileGenerator.py
@@ -24,63 +24,67 @@ def tint(stringToInsert, color):
     return tintedString
 
 functional = ""
-basisSet = "6-31+G*"
-title1 = "Funcs"
-title2 = "Funcs"
-title3 = "Funcs"
-fileData1 = ""
-fileData2 = ""
-fileData3 = ""
-shData1 = ""
-shData2 = ""
-shData3 = ""
-romanNumeral = ""
+basis_set = "6-31+G*"
+titles = []
+file_data = []
+sh_data = []
+roman_numeral = ""
 
 #loads the GJF template from the local directory
-with open("!QChemInput.in", 'r') as file :
-    fileData1 = file.read()
-with open("!QChemInput.in", 'r') as file :
-    fileData2 = file.read()
-with open("!QChemInput.in", 'r') as file :
-    fileData3 = file.read()
-#loads the gaussian.sh job submission template from the local directory
-with open("!gaussianJob.sh", 'r') as file2 :
-    shData1 = file2.read()
-with open("!gaussianJob.sh", 'r') as file2 :
-    shData2 = file2.read()
-with open("!gaussianJob.sh", 'r') as file2 :
-    shData3 = file2.read()
+for i in range(2)
+    with open("!QChemInput.in", 'r') as file :
+        file_data[i] = file.read()
+#loads the qchem.in job submission template from the local directory
+    with open("!qchemJob.sh", 'r') as file_2 :
+        sh_data[i] = file_2.read()
 
-isConfiguring = True
-while isConfiguring:
-    print(tint("Default Functional: ", bcolors.CYAN) + functional)
-    print(tint("Set Gaussian Functional for testing: \nType 'ok' to proceed with the default.", bcolors.LIGHT_CYAN))
+#prompts the user to configure their settings
+is_configuring = True
+while is_configuring:
+    print(tint("Default Functional: ", 
+               bcolors.CYAN) + functional)
+    print(tint("Set Q-Chem Functional for testing: "
+               + "\nType 'ok' to proceed with the default.", 
+               bcolors.LIGHT_CYAN))
     
-    theInput = str(input())
-    if(theInput.lower() != "ok"):
-        functional = theInput
-    print(tint("Default Roman Numeral: ", bcolors.CYAN) + romanNumeral)
-    print(tint("Set Roman Numeral to avoid repeat titles. \nType 'ok' to proceed with the default.", bcolors.LIGHT_CYAN))
+    the_input = str(input())
+    if(the_input.lower() != "ok"):
+        functional = the_input
+    print(tint("Default Roman Numeral: ", 
+               bcolors.CYAN) + roman_numeral)
+    print(tint("Set Roman Numeral to avoid repeat titles. " + 
+               "\nType 'ok' to proceed with the default.", 
+               bcolors.LIGHT_CYAN))
     
-    theInput = str(input())
-    if(theInput.lower() != "ok"):
-        romanNumeral = theInput.upper()
+    the_input = str(input())
+    if(the_input.lower() != "ok"):
+        romanNumeral = the_input.upper()
     
-    print(tint("Current Settings: ", bcolors.CYAN))
+    print(tint("Current Settings: ", 
+               bcolors.CYAN))
     print("Functional: " + functional)
-    print("Basis Set: " + basisSet)
-    print("Roman Numeral: " + romanNumeral)
+    print("Basis Set: " + basis_set)
+    print("Roman Numeral: " + roman_numeral)
     
-    print(tint("Set Gaussian Functional for testing. \nType 'ok' to generate your .gjf's.", bcolors.LIGHT_CYAN))
-    theInput = str(input())
-    if(theInput.lower() == "ok"):
-        title1 += functional + "-" + romanNumeral + "-" + "1st"
-        title2 += functional + "-" + romanNumeral + "-" + "2nd"
-        title3 += functional + "-" + romanNumeral + "-" + "3rd"
+    print(tint("Set Q-Chem Functional for testing." +
+               "\nType 'ok' to generate your .gjf's.", 
+               bcolors.LIGHT_CYAN))
+    the_input = str(input())
+    if(the_input.lower() == "ok"):
+        for i in range(2)
+            titles[i] = "Qfuncs"
+            titles[i] += functional + "-" + romanNumeral + "-"
+            
+            if (i == 0):
+                titles[i] += "1st"
+                elif (i == 1):
+                    titles[i] += "2nd"
+                    elif (i == 2):
+                        titles[i] += "3rd"
+            
         
-        fileData1 = fileData1.replace('TITLE', title1)
-        fileData2 = fileData2.replace('TITLE', title2)
-        fileData3 = fileData3.replace('TITLE', title3)
+            file_data[i] = file_data[i].replace(
+                'TITLE', titles[i])
         
         fileData1 = fileData1.replace('ROOT', 'root=1')
         fileData2 = fileData2.replace('ROOT', 'root=2')
@@ -90,9 +94,9 @@ while isConfiguring:
         fileData2 = fileData2.replace('FUNCTIONAL', functional)
         fileData3 = fileData3.replace('FUNCTIONAL', functional)
         
-        fileData1 = fileData1.replace('BASIS_SET', basisSet)
-        fileData2 = fileData2.replace('BASIS_SET', basisSet)
-        fileData3 = fileData3.replace('BASIS_SET', basisSet)
+        fileData1 = fileData1.replace('BASIS_SET', basis_set)
+        fileData2 = fileData2.replace('BASIS_SET', basis_set)
+        fileData3 = fileData3.replace('BASIS_SET', basis_set)
         
         shData1 = shData1.replace('TITLE', title1)
         shData2 = shData2.replace('TITLE', title2)
